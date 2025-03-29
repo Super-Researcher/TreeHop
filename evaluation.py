9bcfb5ce2e072dcd0d8460bee150c7cda1490441
Super-Researcher <al457743704@icloud.com>
2025-03-30 07:42:42 +0900

    cleanup evaluation code

--- evaluation.py
+++ evaluation.py
@@ -67,25 +67,6 @@ def get_retriever(dataset_name, model):
     return retriever
 
 
-def old_match_retrieve(df):
-    set_title = df["set_evidence_title"].copy()
-    idx_result = df.name
-
-    lst_match = [0] * len(lst_retrieve)
-    for i_hop, retrieved in enumerate(lst_retrieve):
-        passages = retrieved[idx_result * top_n**i_hop:
-                            idx_result * top_n**i_hop + top_n**i_hop]
-        for psg in passages:
-            for p in psg:
-                if p["title"] not in set_title:
-                    continue
-
-                set_title.remove(p["title"])
-                lst_match[i_hop] += 1
-
-    return lst_match
-
-
 def match_retrieve(df, retrieved_passages):
     set_title = df["set_evidence_title"].copy()
     idx_result = df.name
@@ -141,41 +122,26 @@ def evaluate_dataset(
 def parse_args():
     parser = argparse.ArgumentParser(description="Evaluate TreeHop")
 
-    # multihop retrieval with TreeHop
-    parser.add_argument(
-        "--n_hop", type=int,
-        help="Number of hops for multihop retrieval"
-    )
     parser.add_argument(
         "--dataset_name",
         type=str,
         choices=["2wiki", "musique", "multihop_rag"],
+    )
+
+    # for multihop retrieval
+    parser.add_argument(
+        "--n_hop", type=int,
+        help="Number of hops for multihop retrieval"
     )
     parser.add_argument(
         "--top_n", type=int, default=5,
         help="Number of retrieved chunks for each hop"
     )
 
-    # TreeHop model and retrieval settings
+    # retrieval model and TreeHop settings
     parser.add_argument(
         "--state_dict", type=str,
         help="Resume with saved parameters"
-    )
-    parser.add_argument(
-        "--g_size", type=int, default=2048,
-        help="Gate size"
-    )
-    parser.add_argument(
-        "--mlp_size", type=int, default=2048,
-        help="MLP layer size"
-    )
-    parser.add_argument(
-        "--n_mlp", type=int, default=3,
-        help="Number of sequential MLP layers"
-    )
-    parser.add_argument(
-        "--n_head", type=int, default=1,
-        help="Number of stacked node modules"
     )
     parser.add_argument(
         "--index_batch_size", type=int, default=10240,
@@ -201,36 +167,11 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
 
-    # n_hop = 2
-    # top_n = 10
-    # redundant_pruning = True
-    # layerwise_top_pruning = True
-    # index_batch_size=10240
-    # generate_batch_size=1024
-
-    # num_negatives = 5
-    # x_size = 1024
-    # g_size = 2048
-    # mlp_size = 2048
-    # n_mlp = 3
-    # dropout = 0.1
-    # n_heads = 1
-    # pt_file = "checkpoint/treehop_2wiki=0.123&musique=0.027&multihop_rag=0.093__epoch=9&n_neg=5&neg_mode=paired&g_size=2048&mlp_size=2048&n_mlp=3&n_head=1&dropout=0.1&batch_size=64&lr=6e-05&temperature=0.15&weight_decay=2e-08.pt"
-    # dataset_name = "2wiki"  # hotpotqa 2wiki musique multihop_rag
-
     model = get_evaluate_model(args.state_dict)
-
-    # with open("eval_data/retrieve_tree_hop.pkl", "rb") as f:
-    #     retrieved_passages = pickle.load(f)
-    with open("eval_data/retrieve_graph_reranker.pkl", "rb") as f:
-        lst_retrieve = pickle.load(f)
-
 
     df_QA = get_dataset(args.dataset_name)
     retriever = get_retriever(args.dataset_name, model)
-    # retriever.tree_hop_model = model.eval()
-    # res = retriever.search_passages(df_QA["question"].to_list(), top_n=top_n)
-    # retrieved_passages = [res[0]]
+
     retrieved_result = retriever.multihop_search_passages(
         df_QA["question"].to_list(),
         n_hop=args.n_hop,
@@ -251,7 +192,6 @@ if __name__ == '__main__':
         result_type="expand"
     )
     df_match = pd.concat([df_QA, df_match], axis=1)
-    # df_match = df_match[~df_match["type"].isin(["comparison", "null_query"])]
     n_total = df_match["set_evidence_title"].map(len).sum()
 
     print("Recall on each iteration:")
@@ -266,3 +206,9 @@ if __name__ == '__main__':
     for i, psgs in enumerate(retrieved_result.passage):
         k += sum(map(lambda x: len(x), psgs))
         print(f"Avg. K on hop {i+1}:", k / len(psgs))
+
+    # with open(f"eval_data/{args.dataset_name}_result__top_n={args.top_n}&n_hop={args.n_hop}&redundant={args.redundant_pruning}&layerwise_top={args.layerwise_top_pruning}.pkl", "wb") as f:
+    #     pickle.dump(retrieved_result, f)
+
+    # with open("eval_data/retrieve_tree_hop.pkl", "rb") as f:
+    #     retrieved_passages = pickle.load(f)
